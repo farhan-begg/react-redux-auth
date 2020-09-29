06b6607687e512006d1d74a937b92a06ecb64689
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-09-29 01:18:09 -0700

    setup actions

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+
+// Actions types
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// Action Creators
+export const incrementCount = () => {
+    return {
+        type: INCREMENT,
+    }
+}
+
+export const decrementCount = () => {
+    return {
+        type: DECREMENT
+    }
+}
+
